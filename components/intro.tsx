d4b4067b1f618f8cd3dcc6a7fcc7667edb7e6355
Mike Starr <imhs@protonmail.com>
2023-10-16 14:08:33 -0400

    fix button padding

--- components/intro.tsx
+++ components/intro.tsx
@@ -116,7 +116,7 @@ export default function Intro() {
             <FaGithubSquare />
           </a>
           <a
-            className="bg-white p-3 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white py-3 px-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://mirror.xyz/starrdev.eth"
             target="_blank"
           >
